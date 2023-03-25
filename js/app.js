f04315df28df12d59a8478ac98fb91305538318e
Cody <cjoel91@hotmail.com>
2023-03-25 16:58:46 -0600

    W9A Updates Tweets

--- js/app.js
+++ js/app.js
@@ -12,17 +12,44 @@ while (counter2 >= -50) {
 counter1 = 0
 let users = ['Alex', 'Cat', 'CatAlex'];
 let counter = 0;
-while(counter < users.length) {
-    console.log(users[counter]);
-    counter = counter +1;
+while (counter < users.length) {
+  console.log(users[counter]);
+  counter = counter + 1;
 }
 let company = {
-    name: "Acme Corp",
-    yearly_revenue: 1500000,
-    ceo: "John Smith",
-    number_of_employees: 100,
-    managers: ["Mary Jones", "Bob Smith", "Samantha Lee"]
+  name: "Acme Corp",
+  yearly_revenue: 1500000,
+  ceo: "John Smith",
+  number_of_employees: 100,
+  managers: ["Mary Jones", "Bob Smith", "Samantha Lee"]
+}
+
+const tweets = [
+  {
+    tweet: "Hello world!",
+    username: "JohnDoe",
+    created_at: "03-22-2023",
+    age: 25
+  },
+  {
+    tweet: "This is a tweet",
+    username: "JaneDoe",
+    created_at: "03-20-2023",
+    age: 17
+  },
+
+];
+
+let i = 0;
+while (i < tweets.length) {
+  if (tweets[i].age >= 18) {
+    console.log(tweets[i].tweet + " - " + tweets[i].username + " - " + tweets[i].created_at);
   }
-  
+  i++;
+}
 
-
+for (let j = 0; j < tweets.length; j++) {
+  if (tweets[j].age < 18) {
+    console.log(tweets[j].tweet + " - " + tweets[j].username + " - " + tweets[j].created_at);
+  }
+}
