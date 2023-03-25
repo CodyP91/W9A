05f3fc6e66c4552d48bc4c4bd0ff3c63b92fea6e
Cody <cjoel91@hotmail.com>
2023-03-25 16:54:54 -0600

    Updates W9A Should work now

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
-let item_names = ["Item 1", "Item 2", "Item 3"];
-let item_prices = [10, 20, 30];
-let in_stock = [true, false, true];
+let counter1 = 0;
+while (counter1 <= 50) {
+  console.log(counter1);
+  counter1 = counter1 + 1;
+}
 
-let first_item_name = item_names[0];
-let first_item_price = item_prices[0];
-
-item_names.push("Item 4");
-item_prices.push(40);
-in_stock.push(true);
-
-let last_item_name = item_names.pop();
-let last_item_price = item_prices.pop();
-let last_item_stock = in_stock.pop();
+let counter2 = 0;
+while (counter2 >= -50) {
+  console.log(counter2);
+  counter2 = counter2 - 1;
+}
+counter1 = 0
+let users = ['Alex', 'Cat', 'CatAlex'];
+let counter = 0;
+while(counter < users.length) {
+    console.log(users[counter]);
+    counter = counter +1;
+}
+let company = {
+    name: "Acme Corp",
+    yearly_revenue: 1500000,
+    ceo: "John Smith",
+    number_of_employees: 100,
+    managers: ["Mary Jones", "Bob Smith", "Samantha Lee"]
+  }
+  
 
 
